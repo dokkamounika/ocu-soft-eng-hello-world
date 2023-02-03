38219830d0d90d7f6d306d52a5be7fd4886b8615
bpallreddy <bpallreddy@my.okcu.edu>
2023-02-03 10:47:14 +0530

    final comit

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maxwell");
         System.out.println("Hello Kyle Suder");
+        System.out.println("Hello Bindu Pallreddyr");
+
+
     }
 }
