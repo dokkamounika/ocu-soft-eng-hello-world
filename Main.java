dfdc663bbe2dcf389744a80fa424b8f9db9f7cd7
bpallreddy <bpallreddy@my.okcu.edu>
2023-02-03 22:07:14 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Maxwell");
-        System.out.println("Hello Kyle Suder");
-	System.out.println("Hello Chung-Yi Ma");
+        System.out.println("Hello Bindu Pallreddy");
+        System.out.println("Bindu created new Branch Pallreddy");
     }
 }
