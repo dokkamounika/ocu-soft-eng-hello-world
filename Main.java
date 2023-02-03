1fd5a1a6f89c24e1be7c20daacaafbdb91fb4593
“subhashbudati7” <subhashbudati1998@gmail.comgit config --global user.email subhashbudati1998@gmail.com>
2023-02-03 11:01:53 -0600

    Adding name

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maxwell");
         System.out.println("Hello Kyle Suder");
-	System.out.println("Hello Chung-Yi Ma");
+	    System.out.println("Hello Chung-Yi Ma");
+        System.out.println("Hello Subhash Budati");
     }
 }
