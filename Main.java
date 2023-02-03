ec508141a9ea3728a1004534903175aaae3b2be7
bpallreddy <124187381+bpallreddy@users.noreply.github.com>
2023-02-04 02:59:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maxwell");
         System.out.println("Hello Kyle Suder");
-	    System.out.println("Hello Chung-Yi Ma");
+	System.out.println("Hello Chung-Yi Ma");
         System.out.println("Hello Subhash Budati");
         System.out.println("Hello Sumanth Bachineni");
-    }
+	System.out.println("Hello Bindu Pallreddy");    
+    }  
+	
 }
