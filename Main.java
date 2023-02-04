070da375c5a64beecfd5b2beeaf68b7df9a9bde4
AmithReddyBurri <aburri@my.okcu.edu>
2023-02-04 00:38:45 -0600

    add

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maxwell");
         System.out.println("Hello Kyle Suder");
-	System.out.println("Hello Chung-Yi Ma");
+        System.out.println("Hello Chung-Yi Ma");
         System.out.println("Hello Subhash Budati");
         System.out.println("Hello Sumanth Bachineni");
-	System.out.println("Hello Bindu Pallreddy"); 
-    System.out.println("Hello chaitanya machineni"); 
+        System.out.println("Hello Bindu Pallreddy");
+        System.out.println("Hello chaitanya machineni");
+        System.out.println("Hello Sai krishna Chiliveri");
 
-    }  
-	
+    }
+
 }
